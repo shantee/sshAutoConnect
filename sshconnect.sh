a5f2715058e0369b1eec527c37adfad88285eb0a
Philippe BENOIST <shantee@users.noreply.github.com>
2021-02-25 15:33:42 +0100

    Update sshconnect.sh

--- sshconnect.sh
+++ sshconnect.sh
@@ -1,17 +1,16 @@
- #!/bin/sh
+ #!/bin/bash
  # I made this script to rapidly connect to an ssh server.  
  # It is indeed very convenient (at least to me , I use it almost every day) 
  # but not very wise or safe, in term of security , since longi and password must be stored
  # as variable inside the script. use it wisely
  
 # SHH connections informations  password, login , server (ip), port
-sshmdp=enteryourpasswordhere!;
+sshmdp='enteryourpasswordhere!';
 sshlogin=yourlogin;
 sshserver=192.168.1.1; #the server ip / hostname
 sshport=22 #your ssh port
 
-
-# some colors
+# some colors
 red="\e[0;91m"
 blue="\e[0;94m"
 expand_bg="\e[K"
@@ -26,7 +25,7 @@ uline="\e[4m"
 
 if ! command -v sshpass &> /dev/null
 then
-    echo -ne "sshpass is not installed\n"
+    echo -e "sshpass is not installed\n"
     echo "install it ! (ie: sudo apt install sshpass)"
     exit
 fi
@@ -36,6 +35,6 @@ echo -e "${blue_bg}       connecting to $sshserver       ${reset}"
 echo -e "${red_bg}!  !  !   ATTENTION !! WARNING !  !  ! ${reset}\n"
 echo -e "${white}${bold}Remember that the password and login are stored inside this script.${reset}"
 echo -e "So....Yep! it's not very secure (but pretty convenient though :)"
-echo -e "${green}Press any key when ready ! ${reset}" 
-read -p "" -n1 -s
+echo -e "${green}Press any key when ready ! ${reset}";
+read -n 1;
 sshpass -f <(printf '%s\n' $sshmdp) ssh $(echo $sshlogin)@$(echo $sshserver) -p $(echo $sshport)
